14ac361a5f08753a5ca5bd1be0fba10d9324f59e
xiaoyao <xiaoyao.working@gmail.com>
2017-09-13 18:30:26 +0800

    Blade模板引擎应用Lee框架

---
+++ src/Autoloader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lee\Blade;
+
+class Autoloader
+{
+    /**
+     * Registers Lee\Blade\Autoloader as an SPL autoloader and require helpers function.
+     */
+    public static function register()
+    {
+        require 'helpers.php';
+
+        spl_autoload_register([self::class, 'autoload']);
+    }
+
+    /**
+     * Handles autoloading of classes.
+     *
+     * @param string $class a class name
+     */
+    public static function autoload($class)
+    {
+        if (0 !== strpos($class, 'Lee\Blade')) {
+            return;
+        }
+        if (is_file($file = dirname(__FILE__).'/'.str_replace('\\', '/', substr($class, 13)).'.php')) {
+            require $file;
+        }
+    }
+}
---
+++ src/Contracts/Renderable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lee\Blade\Contracts;
+
+interface Renderable
+{
+    /**
+     * Get the evaluated contents of the object.
+     *
+     * @return string
+     */
+    public function render();
+}
